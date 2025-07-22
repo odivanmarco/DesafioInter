371a0827b3e268af8543c2b1a60d15e8970c3412
odivanmarco <odivanmarcos2@gmail.com>
2025-07-22 14:06:18 -0300

    feat: add getUser method to UserService and implement user retrieval with wallet balances

--- transfer/src/main/java/fx/wallet/infra/input/controller/UserController.java
+++ transfer/src/main/java/fx/wallet/infra/input/controller/UserController.java
@@ -6,9 +6,12 @@ import fx.wallet.core.service.UserService;
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.annotation.Body;
 import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.Post;
 
 import static io.micronaut.http.HttpResponse.created;
+import static io.micronaut.http.HttpResponse.noContent;
+import static io.micronaut.http.HttpResponse.ok;
 
 
 @Controller("/users")
@@ -23,6 +26,15 @@ public class UserController {
     @Post
     public HttpResponse<UserResponseDTO> createUser(@Body UserRequestDTO userDTO) {
         return created(userService.createUser(userDTO));
+    }
+
+    @Get("/{id}")
+    public HttpResponse<UserResponseDTO> getUser(String id) {
+        try {
+            return ok(userService.getUser(id));
+        } catch (Exception e) {
+            return noContent();
+        }
     }
 
 } 
