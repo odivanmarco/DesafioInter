eb7f97d32ef8775a3443fb91c540bb0c5359e75d
odivanmarco <odivanmarcos2@gmail.com>
2025-07-22 13:24:40 -0300

    feat: create new exceptions

---
+++ transfer/src/main/java/fx/wallet/core/exception/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package fx.wallet.core.exception;
+
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
