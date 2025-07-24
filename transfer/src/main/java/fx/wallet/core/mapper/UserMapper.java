2ab993775594cf3e9744a0628c09fccbefafa2e1
odivanmarco <odivanmarcos2@gmail.com>
2025-07-24 15:15:29 -0300

    feat: refactor UserMapper to generate UUID and ignore balance fields in response DTO

--- transfer/src/main/java/fx/wallet/core/mapper/UserMapper.java
+++ transfer/src/main/java/fx/wallet/core/mapper/UserMapper.java
@@ -11,18 +11,24 @@ import java.util.UUID;
 
 import static fx.wallet.ApplicationConstants.PJ_USER_TYPE;
 
-@Mapper(componentModel = "jsr330", imports = UUID.class)
+@Mapper(componentModel = "jsr330")
 public interface UserMapper {
 
-    @Mapping(target = "id", expression = "java(UUID.randomUUID())")
+    @Mapping(target = "id", expression = "java(generateId())")
     @Mapping(target = "dailyLimit", expression = "java(setDailyLimit(userDTO.userType()))")
     User toEntity(UserRequestDTO userDTO);
 
     UserRequestDTO toRequestDTO(User user);
 
+    @Mapping(target = "balanceBrl", ignore = true)
+    @Mapping(target = "balanceUsd", ignore = true)
     UserResponseDTO toResponseDTO(User user);
 
     default BigDecimal setDailyLimit(String userType) {
         return PJ_USER_TYPE.equals(userType) ? BigDecimal.valueOf(50000) : BigDecimal.valueOf(10000);
+    }
+
+    default UUID generateId() {
+        return UUID.randomUUID();
     }
 }
