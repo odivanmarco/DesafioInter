41889b3562c94a30284a4b8ce6472c1287952a9d
odivanmarco <odivanmarcos2@gmail.com>
2025-07-25 10:32:29 -0300

    feat: add constructor to GetQuotationException for simplified message handling

--- transfer/src/main/java/fx/wallet/core/exception/GetQuotationException.java
+++ transfer/src/main/java/fx/wallet/core/exception/GetQuotationException.java
@@ -5,4 +5,8 @@ public class GetQuotationException extends RuntimeException {
     public GetQuotationException(String message, Throwable cause) {
         super(message, cause);
     }
+
+    public GetQuotationException(String message) {
+        super(message);
+    }
 }
